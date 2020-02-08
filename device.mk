15a6f999e7379fe091c529518f6af089ad4651b1
PlayFriik <playfriik@gmail.com>
2020-02-08 15:14:52 +0000

    op3: Introduce Parts
    * Includes settings for Buttons and Touchscreen gestures

--- device.mk
+++ device.mk
@@ -396,6 +396,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Parts
+PRODUCT_PACKAGES += \
+    OnePlusParts
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
